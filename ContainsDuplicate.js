eee211b84df1030a29f0accc732fba730d50dedc
Galit Gerasimov <galit.gerasimov3@gmail.com>
2022-01-01 21:58:59 -0500

    added contains duplicate leetcode challenge

---
+++ ContainsDuplicate.js
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/contains-duplicate/
+
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  return [...new Set(nums)].length === nums.length ? false : true;
+};
